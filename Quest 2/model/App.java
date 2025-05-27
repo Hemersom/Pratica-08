ff4d757b5fc2b9a519d8d43d87ee3b191aa4d498
Hemersom <francisco.emerson@acad.ifma.edu.br>
2025-05-27 10:57:16 -0300

    Adiciona classes de produto e métodos para cadastro e exibição de produtos no sistema

--- Quest 2/model/App.java
+++ Quest 2/model/App.java
@@ -1,7 +1,15 @@
+
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
-        
-    }
+    final int max = 100;
+    Nacional[] nacional = new Nacional[max];
+    int contador_nacional = 0;
+    Importado[] importado = new Importado[max];
+    int contador_importado = 0;
+    Estadual[] estadual = new Estadual[max];
+    int contador_estadual = 0;
+    
     /* MENU
 
 1 – Cadastrar Produto Estadual
@@ -20,7 +28,77 @@ public class App {
 
 9 – SAIR
 */  
-    public static void menu() {
+    public void CadastrarProdutoEstadual(Scanner scanner) {
+        String desc;
+        System.out.print("Digite a descrição do produto estadual: ");
+        desc = scanner.nextLine();
+        scanner.nextLine();
+        double valor;
+        System.out.print("Digite o valor do produto estadual: ");
+        valor = scanner.nextDouble();
+        Estadual produto = new Estadual(desc, valor);
+        if (contador_estadual < max) {
+            estadual[contador_estadual++] = produto;
+            System.out.println("Produto Estadual cadastrado com sucesso!");
+        } else {
+            System.out.println("Limite de produtos estaduais atingido.");
+        }
+    }
+    public void CadastrarProdutoNacional(String desc, double valor) {
+        Nacional produto = new Nacional(desc, valor);
+        if (contador_nacional < max) {
+            nacional[contador_nacional++] = produto;
+            System.out.println("Produto Nacional cadastrado com sucesso!");
+        } else {
+            System.out.println("Limite de produtos nacionais atingido.");
+        }
+    }
+    public void CadastrarProdutoImportado(String desc, double valor) {
+        Importado produto = new Importado(desc, valor);
+        if (contador_importado < max) {
+            importado[contador_importado++] = produto;
+            System.out.println("Produto Importado cadastrado com sucesso!");
+        } else {
+            System.out.println("Limite de produtos importados atingido.");
+        }
+    }
+    public void ExibirProdutosEstaduais() {
+        if (contador_estadual == 0) {
+            System.out.println("Nenhum produto estadual cadastrado.");
+        } else {
+            System.out.println("Produtos Estaduais:");
+            for (int i = 0; i < contador_estadual; i++) {
+                System.out.println(estadual[i]);
+            }
+        }
+    }
+    public void ExibirProdutosNacionais() {
+        if (contador_nacional == 0) {
+            System.out.println("Nenhum produto nacional cadastrado.");
+        } else {
+            System.out.println("Produtos Nacionais:");
+            for (int i = 0; i < contador_nacional; i++) {
+                System.out.println(nacional[i]);
+            }
+        }
+    }
+    public void ExibirProdutosImportados() {
+        if (contador_importado == 0) {
+            System.out.println("Nenhum produto importado cadastrado.");
+        } else {
+            System.out.println("Produtos Importados:");
+            for (int i = 0; i < contador_importado; i++) {
+                System.out.println(importado[i]);
+            }
+        }
+    }
+    public void ExibirTodosProdutos() {
+        ExibirProdutosEstaduais();
+        ExibirProdutosNacionais();
+        ExibirProdutosImportados();
+    }
+     
+    public void exe() {
         System.out.println("+--------------- MENU----------------+");
         System.out.println("1 – Cadastrar Produto Estadual");
         System.out.println("2 – Cadastrar Produto Nacional");
@@ -36,7 +114,8 @@ public class App {
         opcao = scanner.nextInt();
         switch (opcao) {
             case 1:
-                // Cadastrar Produto Estadual
+                
+                CadastrarProdutoEstadual(desc,valor);
                 break;
             case 2:
                 // Cadastrar Produto Nacional
@@ -64,5 +143,8 @@ public class App {
                 System.out.println("Opção inválida. Tente novamente.");
                 break;
         }
+    }
+    public static void main(String[] args) {
+        
     }  
 }
